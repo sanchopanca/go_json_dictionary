364855000ce6e4b49ed4eab1d82bd06029adea11
Alexander Kovalev <sanchopanca.alone@gmail.com>
2014-09-03 16:26:53 +0400

    Tabs -> Spaces

--- dict.go
+++ dict.go
@@ -15,24 +15,24 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getJson(word string) string {
-	rows, err := global_db.Query("select definition from dictionary where word = ?", word)
-	if err != nil {
-		log.Fatal(err)
-		return "{}"
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var definition string
-		rows.Scan(&definition)
-		return fmt.Sprintf("{\"%s\": \"%s\"}", word, definition)
-	}
-	rows.Close()
-	return "{}"
+    rows, err := global_db.Query("select definition from dictionary where word = ?", word)
+    if err != nil {
+        log.Fatal(err)
+        return "{}"
+    }
+    defer rows.Close()
+    for rows.Next() {
+        var definition string
+        rows.Scan(&definition)
+        return fmt.Sprintf("{\"%s\": \"%s\"}", word, definition)
+    }
+    rows.Close()
+    return "{}"
 }
 
 func main() {
-	global_db, _ = sql.Open("sqlite3", "./dict.db")
-	defer global_db.Close()
+    global_db, _ = sql.Open("sqlite3", "./dict.db")
+    defer global_db.Close()
 
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
